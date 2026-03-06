3bb28bf9cb097dc9eeb9bd90de647ed9f4b78fef
emmanuel <anyebeemmanuel08@gmail.com>
2026-03-06 09:38:29 -0800

    Added Google verification meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,3 +42,20 @@ export const metadata = {
     type: "website",
   },
 };
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="6VJHL4FigtYxmuFtZdHqsbseLcaPhatG8cifoQucyF4"
+        />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
